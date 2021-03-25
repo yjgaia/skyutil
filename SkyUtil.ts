c494abd84ba94c6fee6a31639ccbf61fedc0ed52
hanul <hanul@hanul.me>
2021-03-25 10:06:29 +0900

    add random

--- SkyUtil.ts
+++ SkyUtil.ts
@@ -1,10 +1,15 @@
 export default class SkyUtil {
-    public static pull(array: any[], ...removeList: any[]) {
+
+    public static pull(array: any[], ...removeList: any[]): void {
         for (const el of removeList) {
             const index = array.indexOf(el);
             if (index !== -1) {
                 array.splice(index, 1);
             }
         }
+    }
+
+    public static random(min: number, max: number): number {
+        return Math.floor(Math.random() * (max - min + 1) + min);
     }
 }
